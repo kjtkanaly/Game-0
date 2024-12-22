10a60dfec85aa321ef85e01a4124a3ade866f599
kjtkanaly <kjtkanaly@gmail.com>
2024-12-22 14:42:35 -0500

    Got the init enviorment setup. Added a Vector2 struct and a basic Object class

--- GameZero.cpp
+++ GameZero.cpp
@@ -5,19 +5,35 @@
 
 using namespace std;
 
-const int WIDTH = 960, HEIGHT = 720;
+// -----------------------------------------------------------------------------
+// GENERAL TOOLS
+// Useful data type
+struct Vector2
+{
+    float x;
+    float y;
+
+    Vector2(float inX, float inY):
+        x(0),
+        y(0)
+    {
+        x = inX;
+        y = inY;
+    }
+};
+// -----------------------------------------------------------------------------
 
 // -----------------------------------------------------------------------------
-// Engine Stuff
-// -----------------------------------------------------------------------------
-
+// ENGINE TOOLS
 class SDL_General
 {
-public: 
-    int testVal;
+public:
+    SDL_Window* window;
+    SDL_Renderer* rend;
 
     SDL_General():
-        testVal(420)
+        window(NULL),
+        rend(NULL)
     {}
 
     void Init()
@@ -32,7 +48,7 @@ public:
                   << std::endl;
     }
 
-    SDL_Window* CreateWindow(
+    void CreateWindow(
         const char* title, 
         const int& posX,
         const int& posY,
@@ -41,7 +57,7 @@ public:
         const Uint32& flags) 
     {
         // Create the SDL window
-        SDL_Window* window = SDL_CreateWindow(
+        window = SDL_CreateWindow(
             title,
             posX,
             posY,
@@ -50,45 +66,67 @@ public:
             flags);
 
         // Window Error Catch
-        if (window == NULL) {
+        if (window == NULL) 
+        {
             SDL_Quit();
             std::cerr << "Error Creating Window: " 
                       << SDL_GetError() 
                       << std::endl;
         }
-
-        return window;
     }
 
-    SDL_Renderer* CreateRenderer(
-        SDL_Window* window,
+    void CreateRenderer(
         const Uint32& renderFlags)
     {
+        // Error Catch
+        if (window == NULL) 
+        {
+            SDL_Quit();
+            std::cerr << "No Window defined to attach the renderer too"
+                      << std::endl;
+        }
+
         // Create the Render Pointer
-        SDL_Renderer* rend = SDL_CreateRenderer(window, -1, renderFlags);
+        rend = SDL_CreateRenderer(window, -1, renderFlags);
 
         // Render Error Catch
-        if (!rend) {
+        if (!rend) 
+        {
             SDL_DestroyWindow(window);
             SDL_Quit();
             std::cerr << "Error Creating the renderer: " 
                       << SDL_GetError() 
                       << std::endl;
         }
-        
-        return rend;
+
+        // Clear the Window by setting it black
+        SDL_RenderClear(rend);
     }
 
     SDL_Texture* LoadTexture(
-        const char* filePath,
-        SDL_Renderer* rend,
-        SDL_Window* window)
+        const char* filePath)
     {
+        if (window == NULL) 
+        {
+            SDL_Quit();
+            std::cerr << "No Window Defined"
+                      << std::endl;
+        }
+
+        if (rend == NULL) 
+        {
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            std::cerr << "No Renderer Defined"
+                      << std::endl;
+        }
+
         // Load the image into main memory using the SDL library
         SDL_Surface* surf = IMG_Load(filePath);
 
         // If the texture file path wasn't found
-        if (!surf) {
+        if (!surf) 
+        {
             SDL_DestroyRenderer(rend);
             SDL_DestroyWindow(window);
             SDL_Quit();
@@ -104,7 +142,8 @@ public:
         SDL_FreeSurface(surf);  
 
         // If the texture wasn't loaded into graphical memory correctly
-        if (!tex) {
+        if (!tex) 
+        {
             SDL_DestroyRenderer(rend);
             SDL_DestroyWindow(window);
             SDL_Quit();
@@ -115,62 +154,110 @@ public:
 
         return tex;
     }
+
+    void Quit() 
+    {
+        // Quit the SDL
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
 };
+// -----------------------------------------------------------------------------
 
-int main() {
-    
-    // Make the general SDL object
-    SDL_General gen;
+// -----------------------------------------------------------------------------
+// GLOBALS
+const int WIDTH = 960, HEIGHT = 720, FRAME_RATE = 60;
+const float SPEED = 300;
+SDL_General GLOBAL_GEN;
+// -----------------------------------------------------------------------------
 
+// -----------------------------------------------------------------------------
+// Game Object
+class Object : public SDL_Rect{
+    public:
+    SDL_Texture* tex;
+
+    Object(
+        const Vector2& inPos = Vector2(0, 0),
+        const char* spriteFile = NULL)
+    {
+        x = inPos.x;
+        y = inPos.y;
+
+        if (spriteFile != NULL) {
+            // Load in the following texture
+            tex = GLOBAL_GEN.LoadTexture(spriteFile);
+
+            // Get the dimensions of the sprite image
+            SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+        }
+    }
+
+    void Process(const float& deltaTime)
+    {
+        
+    }
+};
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// MAIN
+int main() 
+{
     // Init the SDL enviroment
-    gen.Init();
+    GLOBAL_GEN.Init();
 
     // Create the game Window
-    SDL_Window* window = gen.CreateWindow(
+    GLOBAL_GEN.CreateWindow(
         "Hello SDL!",
         0,
         0,
         WIDTH,
         HEIGHT,
-        SDL_WINDOW_ALLOW_HIGHDPI);
+        0);
 
     // Create the renderer for the game window
-    SDL_Renderer* rend = gen.CreateRenderer(
-        window,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    
-    // Load in the following texture
-    SDL_Texture* tex = gen.LoadTexture(
-        "resources/ship-01.png",
-        rend,
-        window);
+    GLOBAL_GEN.CreateRenderer(SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    // Clear the Window by setting it black
-    SDL_RenderClear(rend);
+    // Create the ship object
+    Object ship = Object(Vector2(312, 595), "resources/ship-01.png");
+    ship.w *= 3;
+    ship.h *= 3;
 
-    // Draw the image to the window
-    SDL_RenderCopy(rend, tex, NULL, NULL);
-
-    // Swaps the render from the back buffer to the front
-    SDL_RenderPresent(rend);
+    // Set to 1 when close window button pressed
+    int closeRequested = 0;
 
     // Main Loop
-    SDL_Event event;
-    while (true) {
-        SDL_PollEvent(&event);
+    while (!closeRequested) 
+    {
+        float deltaTime = 1 / ((float) FRAME_RATE);
 
-        if (event.type == SDL_QUIT) {
-            break;
+        // Process Events
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    closeRequested = 1;
+                    break;
+            }
         }
-        else if (event.type == SDL_KEYDOWN) {
-            break;
-        }
+
+        // Clear the Window by setting it black
+        SDL_RenderClear(GLOBAL_GEN.rend);
+
+        // Draw the ship to the render window
+        SDL_RenderCopy(GLOBAL_GEN.rend, ship.tex, NULL, &ship);
+
+        // Swaps the render from the back buffer to the front
+        SDL_RenderPresent(GLOBAL_GEN.rend);
+
+        // Wait frame delay
+        SDL_Delay(1000 / ((float) FRAME_RATE));
     }
 
-    // Quit the SDL
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    GLOBAL_GEN.Quit();
 
     return 0;
 }
+// -----------------------------------------------------------------------------
